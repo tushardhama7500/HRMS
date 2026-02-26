76f2bb19aac51adc97eca5159247f69f3df45ad2
tushardhama7500 <tushardhama3@gmail.com>
2026-02-27 00:53:06 +0530

    Fix | add admin url

--- backend/hrms_backend/urls.py
+++ backend/hrms_backend/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls), 
+
     path('api/employees/', views.employee_list_create),
     path('api/employees/<int:pk>/', views.employee_detail),
     path('api/attendance/', views.mark_attendance),
