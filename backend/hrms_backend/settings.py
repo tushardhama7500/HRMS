9e115f39ed8c982f8aea1a1bafb5e3f4e5543643
tushardhama7500 <tushardhama3@gmail.com>
2026-02-27 10:53:14 +0530

    Switching to PostgreSQL for production

--- backend/hrms_backend/settings.py
+++ backend/hrms_backend/settings.py
@@ -53,9 +53,9 @@ TEMPLATES = [
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True # Render Postgres requires SSL for external connections
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600
+        ssl_require=True
     )
 }
 
